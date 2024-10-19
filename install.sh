2789cf9c52e758fa41e3e67b274c65d3ef72110d
mingmingshiliyu <143821597+mingmingshiliyu@users.noreply.github.com>
2024-10-19 10:47:06 +0800

    Update install.sh

---
+++ install.sh
@@ -0,0 +1,46 @@
+# sdkman 
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk version
+sdk list java
+echo "请输入Identifier下的版本"
+read param
+sdk install java $param
+sdk default java $param
+#只在当前窗口生效
+#sdk use java <version>
+sdk current java
+cd ~/.sdkman
+ls .
+echo "it is where java installed,sdk list java and then sdk use java <version> to use java specified version temporary in current terminal"
+
+#homebrew
+/bin/zsh -c”$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)”
+
+# git
+brew install git
+
+#pyenv
+brew install pyenv
+
+# nvm
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+
+echo "if below Catalina,try run : export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+os_version=$(sw_vers -productVersion)
+if [[ $os_version < "10.15" ]]; then
+  export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+elif [[ $os_version >= "10.15" ]]; then
+  export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+fi
+source ~/.bash_profile
+source ~/.zshrc
+nvm ls-remote
+echo "请输入Identifier下的版本"
+read version
+nvm install $version
+nvm use $version
+node -v
