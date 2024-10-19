ef50fc9ac4a6d6fb8037053c87686771aed20b6f
mingmingshiliyu <143821597+mingmingshiliyu@users.noreply.github.com>
2024-10-19 10:51:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-# sdkman 
+function installSDKMAN(){
+  # sdkman 
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk version
@@ -13,18 +14,25 @@ sdk current java
 cd ~/.sdkman
 ls .
 echo "it is where java installed,sdk list java and then sdk use java <version> to use java specified version temporary in current terminal"
-
+}
+function installhomebrew(){
 #homebrew
 /bin/zsh -c”$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)”
+}
 
+function installgit(){
 # git
 brew install git
+}
 
+function installpyenv(){
 #pyenv
 brew install pyenv
+}
 
+function installNode(){
 # nvm
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
 
 echo "if below Catalina,try run : export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 #[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
@@ -44,3 +52,11 @@ read version
 nvm install $version
 nvm use $version
 node -v
+
+}
+
+installSDKMAN
+installhomebrew
+installgit
+installpyenv
+installNode
